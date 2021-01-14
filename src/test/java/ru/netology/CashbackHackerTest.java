b2969bbbbed2236795dd6039aedb2247022e0900
Nikita <trofimov1nv@gmail.com>
2021-01-14 10:28:40 +0300

    Added tests

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -7,12 +7,32 @@ import static org.testng.Assert.*;
 public class CashbackHackerTest {
 
     @Test
-    public void testRemain() {
+    public void shouldReturn100IfAmountIs900() {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int amount = 900;
 
         int actual = cashbackHacker.remain(amount);
         int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 0;
+
+        int actual = cashbackHacker.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn1000IfAmountIs1000() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 1000;
+
+        int actual = cashbackHacker.remain(amount);
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
