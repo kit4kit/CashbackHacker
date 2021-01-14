c79932a56cbe137b1045322d6096d8a49084166e
Nikita <trofimov1nv@gmail.com>
2021-01-14 10:33:15 +0300

    Added tests to branch junit4

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackerTest {
 
     @Test
-    public void testRemain() {
+    public void shouldReturn100IfAmountIs900() {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int amount = 900;
 
@@ -16,6 +16,24 @@ public class CashbackHackerTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 0;
 
+        int actual = cashbackHacker.remain(amount);
+        int expected = 1000;
 
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn1000IfAmountIs1000() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        int amount = 1000;
+
+        int actual = cashbackHacker.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
